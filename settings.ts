b7fa813af9502644b8c6dd08a91787c6babb3613
Sergio Lopez <sergiolopez.mtwdm@gmail.com>
2021-03-05 21:23:24 -0600

    primer commit

---
+++ settings.ts
@@ -0,0 +1,12 @@
+export default{
+    api:{
+        enviroment:'DEV',//DEV| QA | PROD
+        port:3000
+    },
+    mongodb:{
+        host:'172.17.0.4',
+        port:'27017',        
+        userName:'dba-root',
+        password:'mongoadmin'
+    }
+}
